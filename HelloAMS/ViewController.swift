5ee0215304378297cd00c2efc28759c40ef4bd53
Jakub Bogunia <bogunia.jakub@gmail.com>
2019-12-16 08:46:03 -0800

    Initial Commit

---
+++ HelloAMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloAMS
+//
+//  Created by Jakub Bogunia on 16/12/2019.
+//  Copyright © 2019 Jakub Bogunia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
